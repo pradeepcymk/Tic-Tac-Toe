11c4dcc544110fba84f5be444c3399f5ca4d091f
Pradeep kumar <pradeepkumar2025v@gmail.com>
2026-04-21 09:36:56 +0000

    UC2: Toss to decide first player and assign symbols

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,31 +1,44 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    // Initialize board with '-'
-    static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+    // Toss logic
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        int toss = rand.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
     }
 
-    // Print board
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
+    // Display result
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first.");
         }
+
+        System.out.println("Your symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
     }
 }
