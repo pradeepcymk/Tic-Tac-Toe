8df09fd2ebcd8940ef1085160981a2a696168661
Pradeep kumar <pradeepkumar2025v@gmail.com>
2026-04-21 09:50:11 +0000

    UC5: Validate user move with boundary and empty check

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,19 +1,28 @@
 public class TicTacToe {
 
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
     public static void main(String[] args) {
-        int slot = 7;
-
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1)); // test
     }
 
-    // Convert slot → row
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
-    }
+    // Validate move
+    static boolean isValidMove(int row, int col) {
 
-    // Convert slot → column
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
+        // Check boundaries (0–2)
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // Check if cell is empty
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
     }
 }
