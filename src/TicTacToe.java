60302c1844bf10e8d06b9699af5e377bb2ab10c7
Pradeep kumar <pradeepkumar2025v@gmail.com>
2026-04-21 09:29:13 +0000

    UC1: Display empty Tic-Tac-Toe board

---
+++ src/TicTacToe.java
@@ -0,0 +1,31 @@
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    // Initialize board with '-'
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    // Print board
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+}
