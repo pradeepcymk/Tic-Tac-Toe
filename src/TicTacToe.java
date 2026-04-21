be9d6b7668008b058b2663373eaf84a2e2668cd4
Pradeep kumar <pradeepkumar2025v@gmail.com>
2026-04-21 09:42:03 +0000

    UC3: Accept user slot input (1-9)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,44 +1,19 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
 
-    // Toss logic
-    static void tossAndAssignSymbols() {
-        Random rand = new Random();
+    // Read user input
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
 
-        int toss = rand.nextInt(2); // 0 or 1
+        System.out.print("Enter a slot (1-9): ");
+        int slot = sc.nextInt();
 
-        if (toss == 0) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    // Display result
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("You won the toss!");
-            System.out.println("You play first.");
-        } else {
-            System.out.println("Computer won the toss!");
-            System.out.println("Computer plays first.");
-        }
-
-        System.out.println("Your symbol: " + humanSymbol);
-        System.out.println("Computer symbol: " + computerSymbol);
+        return slot;
     }
 }
